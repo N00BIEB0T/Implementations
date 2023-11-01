8c32875d9f2fe9c872445d1038120909be9ca1f6
N00BIEB0T <noobiebotorlovage@gmail.com>
2023-11-01 23:40:18 +0530

    total Occurences using bs

--- IMPLEMENTATION/totOcc.cpp
+++ IMPLEMENTATION/totOcc.cpp
@@ -8,29 +8,29 @@ int firstOcc(int arr[], int n, int key){
     int ans = -1;
 
     while(start <= end){
-
+        
         if(arr[mid] == key){
-
+          
             ans = mid;
-            end = mid -1;   // we need to get the first occurence so we need to traverse the array to the left of our mid
-            
+            end = mid - 1;
+        
         }
 
-        else if( arr[mid] > key) {
+        else if(arr[mid] > key){
             
             end = mid - 1;
         
         }
 
         else{
-
+            
             start = mid + 1;
         
         }
 
-        mid = start + (end - start)/2;
-
-    } 
+        mid = start + (end - start)/2;    
+    
+    }
 
     return ans;
 
@@ -38,34 +38,31 @@ int firstOcc(int arr[], int n, int key){
 
 int lastOcc(int arr[], int n, int key){
 
-    int start = 0, end = n-1;
-    int mid = start + (end - start)/2;
+    int start = 0, end = n - 1;
+    int mid = start +(end - start)/2;
     int ans = -1;
 
     while(start <= end){
-
+        
         if(arr[mid] == key){
-
             ans = mid;
-            start = mid + 1;    //we need to get the last occurence so we need to traverse the array to the right of our mid
-            
+            start = mid + 1;
         }
 
-        else if( arr[mid] > key) {
+        else if(arr[mid] > key){
             
             end = mid - 1;
         
         }
 
         else{
-
+            
             start = mid + 1;
-        
         }
 
-        mid = start + (end - start)/2;
-
-    } 
+        mid = start + (end - start)/2;    
+    
+    }
 
     return ans;
 
@@ -73,9 +70,15 @@ int lastOcc(int arr[], int n, int key){
 
 int main(){
 
-    int arr[11] = {1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 5};
+    int arr[11] = {1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 5};
+
+    int firstIndex = firstOcc(arr, 11, 3);
+    int lastIndex = lastOcc(arr, 11, 3);
+
+    int totOcc = (lastIndex - firstIndex) + 1;
 
     cout << "The first occurence of  3 is at index " << firstOcc(arr, 11, 3) << endl;
     cout << "The last occurence of 3 is at index " << lastOcc(arr, 11, 3) << endl;
+    cout << "The total number of occurences of 3 is " << totOcc << endl;
 
 }
